fff21863611ca8c0c865ea4f90f4e2f82bf573aa
yumekiti <yumekiti120440@gmail.com>
2022-08-28 06:07:20 +0900

    `add` router

--- interface/handler/router.go
+++ interface/handler/router.go
@@ -9,5 +9,9 @@ func InitRouting(
 	e *echo.Echo,
 	taskHandler TaskHandler,
 ) {
-	e.GET("/task", taskHandler.Get())
+	e.GET("/task", taskHandler.GetAll())
+	e.GET("/task/:id", taskHandler.Get())
+	e.POST("/task", taskHandler.Post())
+	e.PUT("/task/:id", taskHandler.Put())
+	e.DELETE("/task/:id", taskHandler.Delete())
 }
